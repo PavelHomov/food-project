de1a454afc1495f6f86b8c6be2336012d79bb007
Павел Хомов <pavelhomov@MacBook-Air-Pavel.local>
2023-03-04 21:51:40 +0300

    fff

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -35,24 +35,6 @@ class Tag(models.Model):
     def __str__(self) -> str:
         return f'{self.name}, {self.color}'
 
-    class Ingredient(models.Model):
-        name = models.CharField(
-            max_length=250,
-            verbose_name='Название ингредиента',
-            db_index=True
-        )
-        measurement_unit = models.CharField(
-            max_length=200,
-            verbose_name='Единица измерения'
-        )
-
-        class Meta:
-            verbose_name = 'Ингредиент'
-            verbose_name_plural = 'Ингредиенты'
-
-        def __str__(self):
-            return f'{self.name}, {self.measurement_unit}'
-
 
 class Ingredient(models.Model):
     """
